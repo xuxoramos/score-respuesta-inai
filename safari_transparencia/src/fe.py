52a0eea2a75f08309ff3811e6fa330e3e4ab7fc0
Diego Villa <villa.lizarraga@gmail.com>
2020-07-14 13:49:22 -0600

    Se agrega la función CalidadRespuesta al modulo fe.py

--- safari_transparencia/src/fe.py
+++ safari_transparencia/src/fe.py
@@ -27,3 +27,47 @@ def RespuestaReal(respuesta, tipo_archivo, cantidad_archivos, rechazo):
         return 'sin respuesta'
     else:
         return respuesta
+
+
+
+def CalidadRespuesta(df, columna_respuesta, nueva_columna):
+    """
+    Función para crear la columna calidad_respuesta que agrupa en:
+        1. satisfactoria
+        2. no respondida
+        3. en proceso
+
+    Args:
+        df: dataframe en el que se creara la columna nueva
+        columna_respuesta: nombre de la columna respuesta
+        nueva_columna: nombre de la nueva columna que se creara que contendra la
+        calidad de la respuesta
+    Return:
+        df: dataframe con la nueva columna de calidad de respusta
+    """
+
+    df.loc[df[columna_respuesta] == 'entrega de informacion en medio electronico', nueva_columna] = 'satisfactoria'
+    df.loc[df[columna_respuesta] == 'notificacion de disponibilidad de informacion', nueva_columna] = 'satisfactoria'
+    df.loc[df[columna_respuesta] == 'notificacion lugar y fecha de entrega', nueva_columna] = 'satisfactoria'
+    df.loc[df[columna_respuesta] == 'notificacion de envio', nueva_columna] = 'satisfactoria'
+    df.loc[df[columna_respuesta] == 'respuesta del solicitante a la notificacion de entrega de informacion con costo', nueva_columna] = 'satisfactoria'
+    df.loc[df[columna_respuesta] == 'respuesta del solicitante a la notificacion de entrega de informacion sin costo', nueva_columna] = 'satisfactoria'
+    df.loc[df[columna_respuesta] == 'notificacion de pago', nueva_columna] = 'satisfactoria'
+    df.loc[df[columna_respuesta] == 'la informacion esta disponible publicamente', nueva_columna] = 'satisfactoria'
+
+
+    df.loc[df[columna_respuesta] == 'no es de competencia de la unidad de enlace', nueva_columna] = 'no respondida'
+    df.loc[df[columna_respuesta] == 'inexistencia de la informacion solicitada', nueva_columna] = 'no respondida'
+    df.loc[df[columna_respuesta] == 'sin respuesta', nueva_columna] = 'no respondida'
+    df.loc[df[columna_respuesta] == 'negativa por ser reservada o confidencial', nueva_columna] = 'no respondida'
+    df.loc[df[columna_respuesta] == 'informacion parcialmente reservada o confidencial', nueva_columna] = 'no respondida'
+    df.loc[df[columna_respuesta] == 'la solicitud no corresponde al marco de la ley', nueva_columna] = 'no respondida'
+    df.loc[df[columna_respuesta] == 'no se dara tramite a la solicitud', nueva_columna] = 'no respondida'
+
+
+    df.loc[df[columna_respuesta] == 'requerimiento de informacion adicional', nueva_columna] = 'en proceso'
+    df.loc[df[columna_respuesta] == 'notificacion de prorroga', nueva_columna] = 'en proceso'
+    df.loc[df[columna_respuesta] == 'respuesta a solicitud de informacion adicional', nueva_columna] = 'en proceso'
+    df.loc[df[columna_respuesta] == 'notificacion de cambio de tipo de solicitud', nueva_columna] = 'en proceso'
+
+    return df
